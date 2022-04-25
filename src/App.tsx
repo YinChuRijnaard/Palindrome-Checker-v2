f89350222b280b22e4dc15e2ec34b1f18eb99fe6
Yin Chu <ycrijnaard@gmail.com>
2022-04-25 21:05:19 +0300

    Initial files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen font-mono">
+      <h1 className="my-8 text-center text-4xl font-bold text-red-600">
+        Palindrome Checker
+      </h1>
+
+      <div className="mx-8 grid space-y-8 text-center text-neutral-200">
+        <label className="italic">
+          Type a word or phrase to check if it's a palindrome (e.g. tacocat)
+        </label>
+        <input
+          type="text"
+          className="mx-auto w-96 border-2 border-neutral-200 bg-inherit p-2 caret-red-600 shadow-md shadow-red-600"
+          placeholder="Type something..."
+        />
+
+        <div className="space-x-8 text-slate-900">
+          <button className="bg-neutral-200 p-2 px-6">Check word</button>
+          <button className="bg-neutral-200 p-2 px-6">Clear history</button>
+        </div>
+
+        <div className="bg-yellow-300">{/* Output will go here */}</div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
